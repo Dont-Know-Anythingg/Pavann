cfcdeb2ce139e8ba80cfba0642085345153a6dc3
Pavan6264a <127077968+Pavan6264a@users.noreply.github.com>
2024-04-03 15:24:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', "-1001898590676")
-auth_grp = environ.get('AUTH_GROUPS')
+AUTH_GROUPS = environ.get('AUTH_GROUPS')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # MongoDB information
